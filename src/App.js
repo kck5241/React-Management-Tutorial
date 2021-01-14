966c588ef97ed861a25ae4880ca1c81956dd1772
username <user@email.mail>
2021-01-14 20:19:56 +0900

    vscode://vscode.github-authentication/did-authenticate?windowid=6&code=a6c9b594a7ea1a70b7e2&state=dc96f519-b652-4152-99d7-073b488ca65e

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import React, { Component } from 'react';
+
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'name',
+  'day': '890914',
+  'gender': 'man',
+  'job': 'progrem'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'name1',
+  'day': '860808',
+  'gender': 'man',
+  'job': 'not'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'name2',
+  'day': '091212',
+  'gender': 'man',
+  'job': 'student'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+          <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          day={c.day}
+          gender={c.gender}
+          job={c.job}
+          />
+          );
+        })
+      }
     </div>
   );
 }
